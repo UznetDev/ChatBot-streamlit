c60e6edd9a51566f889f33b697072f5b845fd929
UznetDev <uznattv@gmail.com>
2024-12-05 15:34:37 +0500

    fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 
-
 st.set_page_config(page_title="Chat Bot",
                    page_icon=":bar_chart:",
                    layout="wide")
@@ -16,14 +15,10 @@ from functions.functions import send_request
 st.markdown(st_style, 
             unsafe_allow_html=True)
 
-token = "WHETJx7Y26kgfm4GYl6CywxT4CH4P7E9TNHWUhxh6gmUV7lXhj"
-
-# cookie_controller.set('access_token', token)
 
 cookie = cookie_controller.get('access_token')
 
 access_token = cookie_controller.get('access_token')
-st.write(access_token)
 st.markdown(footer, unsafe_allow_html=True)
 
 
@@ -31,7 +26,6 @@ if access_token:
     url = f"{BASE_API}/auth/login_with_token?access_token={access_token}"
     res, status = send_request(url)
     if 'id' in res and status == 200:
-        # st.write(res, status)
         from routes.home import app
         app(access_token)
     else:
